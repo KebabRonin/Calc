21f7bdef2d65682fe1da9f499711e53a2819d38e
BRASSAT Alexandru <92524740+KebabRonin@users.noreply.github.com>
2024-03-05 13:03:44 +0200

    ex3 fix + bonus, ramane ex2 *

--- tema1.py
+++ tema1.py
@@ -13,7 +13,19 @@ m += 1
 print(f"Ex1: {u=}, {m=}")
 
 x, y, z = 1.0, u/10, u/10
-print(f"Ex2: {(x + y) + z != x + (y + z)=}")
+print(f"Ex2: {((x + y) + z != x + (y + z))=}")
+### !!! Nu merge inmultirea
+m = -1
+u = 0.1
+
+while 1 * (1 + u) != 1:
+	m -= 1
+	u *= 0.1
+u*=10
+m += 1
+print(f"Ex1: {u=}, {m=}")
+x, y, z = 1.0, 1 + u/10, 1 + u/10
+print(f"Ex2: {((x * y) * z != x * (y * z))=}")
 
 def T(t, a):
 	match t:
@@ -28,7 +40,7 @@ def T(t, a):
 		case 5:
 			return (945 * a - 105 * a ** 3 + a ** 5) / (945 - 420 * a ** 2 + 15 * a ** 4)
 		case 6:
-			return (10395 - 1260 * a ** 3 + 21 * a ** 5) / (10395 - 4725 * a ** 2 + 210 * a ** 4 - a ** 6)
+			return (10395 * a - 1260 * a ** 3 + 21 * a ** 5) / (10395 - 4725 * a ** 2 + 210 * a ** 4 - a ** 6)
 		case 7:
 			return (135135 * a - 17325 * a ** 3 + 378 * a ** 5 - a ** 7) / (135135 - 62370 * a ** 2 + 3150 * a ** 4 - 28 * a ** 6)
 		case 8:
@@ -36,21 +48,39 @@ def T(t, a):
 		case 9:
 			return (34459425 * a - 4729725 * a ** 3 + 135135 * a ** 5 - 990 * a ** 7 + a ** 9) / (34459425 - 16216200 * a ** 2 + 945945 * a ** 4 - 13860 * a ** 6 + 45 * a ** 8)
 
+def test_10_000(f, fverif):
+	nrs = [random.uniform(-math.pi/2, math.pi/2) for _ in range(10_000)]
+	rez = [() for _ in range(10_000)]
+	for i in range(4, 10):
+		for j in range(len(nrs)):
+			err = abs(f(i, j) - fverif(j))
+			if rez[j] == () or rez[j][0] > err:
+				rez[j] = (err, i)
 
-nrs = [random.uniform(-math.pi/2, math.pi/2) for _ in range(10_000)]
-rez = [() for _ in range(10_000)]
+	r = [list(map(lambda x: x[0], filter(lambda x: x[1] == i, rez))) for i in range(4, 10)]
+
+	l = [(sum(r[i - 4])/len(r[i - 4]), i) for i in range(4, 10)]
+	l.sort(key=lambda x: x[0])
+
+	for i in l:
+		print(f"{f.__name__}({i[1]}, a) are eroarea medie {i[0]}")
+	print("=====")
+
+	return l[0][1] # returneaza cea mai buna valoare pentru n
 
 
-for i in range(4, 10):
-	for j in range(len(nrs)):
-		err = abs(T(i, j) - math.tan(j))
-		if rez[j] == () or rez[j][0] > err:
-			rez[j] = (err, i)
+best_tan = test_10_000(T, math.tan)
 
-r = [list(map(lambda x: x[0], filter(lambda x: x[1] == i, rez))) for i in range(4, 10)]
+def S(n, a):
+	return T(n, a) / math.sqrt(1 + (T(n, a) ** 2))
+def C(n, a):
+	return 1 / math.sqrt(1 + (T(n, a) ** 2))
 
-l = [(sum(r[i])/len(r[i]), i) for i in range(4, 10)]
-l.sort(key=lambda x: x[1])
-
-for i in l:
-	print(f"T({i}, a) are eroarea medie {l[i][0]}")
+best_sin = test_10_000(S, math.sin)
+best_cos = test_10_000(C, math.cos)
+print(f"{best_tan=}\n{best_sin=}\n{best_cos=}\n=====")
+nr = random.uniform(-math.pi/2, math.pi/2)
+print(f"{nr=}")
+print(f"T({best_tan}, nr) = {T(best_tan, nr):<12} | {math.tan(nr)=} (err={abs(math.tan(nr) - T(best_tan, nr))})")
+print(f"S({best_sin}, nr) = {S(best_sin, nr):<12} | {math.sin(nr)=} (err={abs(math.sin(nr) - S(best_sin, nr))})")
+print(f"C({best_cos}, nr) = {C(best_cos, nr):<12} | {math.cos(nr)=} (err={abs(math.cos(nr) - C(best_cos, nr))})")
